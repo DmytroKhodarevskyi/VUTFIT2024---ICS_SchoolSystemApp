ebd8b87c7b1b98324af815e7ff40e28633d36842
Dmytro <xkhoda01@vutbr.cz>
2024-02-27 19:33:35 +0100

    class create

---
+++ DAL/DAL/Entities/Student.cs
@@ -0,0 +1,11 @@
+namespace DAL.Entities;
+
+public class Student
+{
+    public Guid Id { get; set; }
+    
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    
+    public string Photo { get; set; }
+}
