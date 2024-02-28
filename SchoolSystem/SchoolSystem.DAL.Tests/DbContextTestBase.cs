2886421f78ea36726f9c4ec27148ec9a0973923a
DmytroFlakac <117589874+DmytroFlakac@users.noreply.github.com>
2024-02-28 18:40:07 +0100

    Adding TestBase

--- SchoolSystem/SchoolSystem.DAL.Tests/DbContextTestBase.cs
+++ SchoolSystem/SchoolSystem.DAL.Tests/DbContextTestBase.cs
@@ -1,12 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using SchoolSystem.DAL;
+using SchoolSystem.DAL.Factories;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using Xunit.Abstractions;
 
-namespace SchoolSystem.DAL.Tests
+namespace SchoolSystem.DAL.Tests;
+
+public class DbContextTestsBase : IAsyncLifetime
 {
-    internal class DbContextTestBase
+    protected DbContextTestsBase(ITestOutputHelper output)
     {
+        DbContextFactory = new DbContextSqLiteFactory(GetType().FullName!, seedTestingData: true);
+
+        SchoolSystemDbContextSUT = DbContextFactory.CreateDbContext();
+    }
+
+    protected IDbContextFactory<SchoolSystemDbContext> DbContextFactory { get; }
+    protected SchoolSystemDbContext SchoolSystemDbContextSUT { get; }
+
+
+    public async Task InitializeAsync()
+    {
+        await SchoolSystemDbContextSUT.Database.EnsureDeletedAsync();
+        await SchoolSystemDbContextSUT.Database.EnsureCreatedAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await SchoolSystemDbContextSUT.Database.EnsureDeletedAsync();
+        await SchoolSystemDbContextSUT.DisposeAsync();
     }
 }
