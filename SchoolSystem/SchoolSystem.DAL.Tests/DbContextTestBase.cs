2af51fed6fc14eae2d76c09007652596aa1663aa
DmytroFlakac <117589874+DmytroFlakac@users.noreply.github.com>
2024-02-28 19:17:04 +0100

    conflict resolving

--- SchoolSystem/SchoolSystem.DAL.Tests/DbContextTestBase.cs
+++ SchoolSystem/SchoolSystem.DAL.Tests/DbContextTestBase.cs
@@ -1,12 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
-<<<<<<< HEAD
-using DAL.;
-using DAL.Factories;
-=======
 using SchoolSystem.DAL;
 using SchoolSystem.DAL.Factories;
->>>>>>> 9915cde2ad2526265306f995fab3881598c7546c
 using Microsoft.EntityFrameworkCore;
 using Xunit;
 using Xunit.Abstractions;
@@ -17,12 +12,7 @@ public class DbContextTestsBase : IAsyncLifetime
 {
     protected DbContextTestsBase(ITestOutputHelper output)
     {
-<<<<<<< HEAD
-        DbContextFactory = new DbContextSQLiteTestingFactory(GetType().FullName!, seedTestingData: true);
-=======
         DbContextFactory = new DbContextSqLiteFactory(GetType().FullName!, seedTestingData: true);
->>>>>>> 9915cde2ad2526265306f995fab3881598c7546c
-
         SchoolSystemDbContextSUT = DbContextFactory.CreateDbContext();
     }
 
