fd35ee99a80ed5624f4ec10266f1043c2f4f3a29
DmytroFlakac <117589874+DmytroFlakac@users.noreply.github.com>
2024-02-29 15:47:25 +0100

    Common Seeds

--- SchoolSystem/SchoolSystem.Common.Tests/Seeds/EvaluationSeeds.cs
+++ SchoolSystem/SchoolSystem.Common.Tests/Seeds/EvaluationSeeds.cs
@@ -3,3 +3,66 @@ using DAL.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace SchoolSystem.Common.Tests.Seeds;
+
+public static class EvaluationSeeds
+{
+    public static readonly EvaluationEntity EmptyEvaluation = new()
+    {
+        Id = default,
+        Score = default,
+        Description = default!,
+    };
+    
+    public static readonly EvaluationEntity Evaluation1 = new()
+    {
+        Id = Guid.Parse("00000000-0000-0000-0000-000000000001"),
+        Score = 2,
+        Description = "Test",
+    };
+    
+    public static readonly EvaluationEntity Evaluation2 = new()
+    {
+        Id = Guid.Parse("00000000-0000-0000-0000-000000000002"),
+        Score = 3,
+        Description = "Test",
+    };
+    
+    public static readonly EvaluationEntity EvaluationEntityWithNoStudAct = Evaluation1 with 
+        { Id = Guid.Parse("00000000-0000-0000-0000-000000000003"), 
+            Student = null,
+            Activity = null };
+    
+    public static readonly EvaluationEntity EvaluationEntityUpdated1 = Evaluation1 with { Id = Guid.Parse("00000000-0000-0000-0000-000000000004"),
+        Student = null,
+        Activity = null };
+    
+    public static readonly EvaluationEntity EvaluationEntityDeleted1 = Evaluation1 with { Id = Guid.Parse("00000000-0000-0000-0000-000000000005"),
+        Student = null,
+        Activity = null };
+    
+    public static readonly EvaluationEntity EvaluationEntityUpdate2 = Evaluation2 with { Id = Guid.Parse("00000000-0000-0000-0000-000000000006"),
+        Student = null,
+        Activity = null };
+    
+    public static readonly EvaluationEntity EvaluationEntityDelete2 = Evaluation2 with { Id = Guid.Parse("00000000-0000-0000-0000-000000000007"),
+        Student = null,
+        Activity = null };
+    
+    static EvaluationSeeds()
+    {
+        Evaluation1.Student = StudentSeeds.Student1;
+        Evaluation2.Student = StudentSeeds.Student1;
+        Evaluation1.Activity = ActivitySeeds.Activity1;
+        Evaluation2.Activity = ActivitySeeds.Activity2;
+    }
+    
+    public static void Seed(ModelBuilder modelBuilder) =>
+        modelBuilder.Entity<EvaluationEntity>().HasData(
+            Evaluation1, Evaluation2 with {Student = null, Activity = null},
+            EvaluationEntityWithNoStudAct,
+            EvaluationEntityUpdated1,
+            EvaluationEntityDeleted1,
+            EvaluationEntityUpdate2,
+            EvaluationEntityDelete2
+            );
+}    
--- SchoolSystem/SchoolSystem.Common.Tests/Seeds/SubjectSeeds.cs
+++ SchoolSystem/SchoolSystem.Common.Tests/Seeds/SubjectSeeds.cs
@@ -32,4 +32,35 @@ public static class SubjectSeeds
             Students = Array.Empty<StudentEntity>(), 
             Activities = Array.Empty<ActivityEntity>()};
     
+    public static readonly SubjectEntity SubjectEntityUpdated = IZP with { Id = Guid.Parse("00000000-0000-0000-0000-0000000000056"),
+        Students = Array.Empty<StudentEntity>(), 
+        Activities = Array.Empty<ActivityEntity>()};
+    public static readonly SubjectEntity SubjectEntityDeleted = IZP with { Id = Guid.Parse("00000000-0000-0000-0000-0000000000045"),
+        Students = Array.Empty<StudentEntity>(), 
+        Activities = Array.Empty<ActivityEntity>()};
+    public  static readonly SubjectEntity SubjectEntityUpdate2 = IUS with { Id = Guid.Parse("00000000-0000-0000-0000-0000000000034"),
+        Students = Array.Empty<StudentEntity>(), 
+        Activities = Array.Empty<ActivityEntity>()};
+    public static readonly SubjectEntity SubjectEntityDelete2 = IUS with { Id = Guid.Parse("00000000-0000-0000-0000-0000000000023"),
+        Students = Array.Empty<StudentEntity>(), 
+        Activities = Array.Empty<ActivityEntity>()};
+
+    static SubjectSeeds()
+    {
+        IZP.Students.Add(StudentSeeds.Student1);
+        IUS.Students.Add(StudentSeeds.Student1);
+        IZP.Activities.Add(ActivitySeeds.Activity1);
+        IUS.Activities.Add(ActivitySeeds.Activity2);
+    }
+    
+    public static void Seed(ModelBuilder modelBuilder) =>
+        modelBuilder.Entity<SubjectEntity>().HasData(
+            IZP, IUS with{ Students = Array.Empty<StudentEntity>(), Activities = Array.Empty<ActivityEntity>()},
+            SubjectEntityWithNoStudAct,
+            SubjectEntityUpdated,
+            SubjectEntityDeleted,
+            SubjectEntityUpdate2,
+            SubjectEntityDelete2
+            );
+    
 }
