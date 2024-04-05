6217c42e6445cd3c669372fe4a3e17a48aefd0c2
Dmytro <xkhoda01@vutbr.cz>
2024-04-05 17:19:56 +0200

    ActivityFacade

---
+++ SchoolSystem/SchoolSystem.BL/Facades/ActivityFacade.cs
@@ -0,0 +1,49 @@
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using SchoolSystem.BL.Models;
+using DAL.Entities;
+using DAL.Enums;
+using DAL.UnitOfWork;
+
+namespace SchoolSystem.BL.Facades;
+
+public class ActivityFacade : CRUDFacade<ActivityEntity, ActivityListModel, ActivityDetailModel>
+{
+    private readonly IMapper _mapper;
+    private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+    
+    public ActivityFacade(IMapper mapper, IUnitOfWorkFactory unitOfWorkFactory) : base(mapper, unitOfWorkFactory)
+    {
+        _mapper = mapper;
+        _unitOfWorkFactory = unitOfWorkFactory;
+    }
+    
+    public async Task<List<ActivityListModel>> GetRoomActivities(Room room)
+    {
+        await using var uow = _unitOfWorkFactory.Create();
+        var dbSet = uow.GetRepository<ActivityEntity>().Get()
+            .Where(x => x.Room == room);
+
+        return await _mapper.ProjectTo<ActivityListModel>(dbSet).ToListAsync().ConfigureAwait(false);
+    }
+    
+    public async Task<List<ActivityListModel>> GetActivitiesAfter(DateTime after)
+    {
+        await using var uow = _unitOfWorkFactory.Create();
+        var dbSet = uow.GetRepository<ActivityEntity>().Get()
+            .Where(x => x.Start > after);
+
+        return await _mapper.ProjectTo<ActivityListModel>(dbSet).ToListAsync().ConfigureAwait(false);
+    }
+    
+    public async Task<List<ActivityListModel>> GetActivitiesByTag(int Tag)
+    {
+        await using var uow = _unitOfWorkFactory.Create();
+        var dbSet = uow.GetRepository<ActivityEntity>().Get()
+            .Where(x => x.Tag == Tag);
+
+        return await _mapper.ProjectTo<ActivityListModel>(dbSet).ToListAsync().ConfigureAwait(false);
+    }
+
+    
+}
