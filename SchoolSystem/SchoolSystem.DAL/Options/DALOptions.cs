87597a52b71581c8434ec1c5d5fd80f396e2bf02
DmytroFlakac <117589874+DmytroFlakac@users.noreply.github.com>
2024-04-25 17:13:07 +0200

    Add Migrator

---
+++ SchoolSystem/SchoolSystem.DAL/Options/DALOptions.cs
@@ -0,0 +1,17 @@
+namespace DAL.Options;
+
+public record DALOptions
+{
+    public required string DatabaseDirectory { get; init; }
+    public string DatabaseName { get; init; } = null!;
+    public string DatabaseFilePath => Path.Combine(DatabaseDirectory, DatabaseName!);
+    /// <summary>
+    /// Deletes database before application startup
+    /// </summary>
+    public bool RecreateDatabaseEachTime { get; init; } = false;
+
+    /// <summary>
+    /// Seeds DemoData from DbContext on database creation.
+    /// </summary>
+    public bool SeedDemoData { get; init; } = false;
+}
