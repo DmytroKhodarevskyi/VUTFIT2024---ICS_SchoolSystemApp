1946f2b775fa0a2511fad3c74ca8d60d2bb0b845
DenyysChernenko <xchern08@stud.fit.vutbr.cz>
2024-04-27 13:38:32 +0200

    feat(ViewModels): Add View Models Subjects

---
+++ SchoolSystem/SchoolSystem.App/ViewModels/Subjects/SubjectDetailViewModel.cs
@@ -0,0 +1,60 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using SchoolSystem.App.Messages;
+using SchoolSystem.App.Services.Interfaces;
+using SchoolSystem.BL.Facades.Interfaces;
+using SchoolSystem.BL.Models;
+
+namespace SchoolSystem.App.ViewModels.Subjects;
+
+[QueryProperty(nameof(Id), nameof(Id))]
+public partial class SubjectDetailViewModel(
+    ISubjectFacade subjectFacade,
+    INavigationService navigationService,
+    IMessengerService messengerService,
+    IAlertService alertService)
+    : ViewModelBase(messengerService), IRecipient<EditMessage>
+{
+    public Guid Id { get; set; }
+    public SubjectDetailedModel? Subject { get; private set; }
+
+    protected override async Task LoadDataAsync()
+    {
+        await base.LoadDataAsync();
+
+        Subject = await subjectFacade.GetAsync(Id);
+    }
+
+    [RelayCommand]
+    private async Task DeleteAsync()
+    {
+        if (Subject is not null)
+        {
+            try
+            {
+                await subjectFacade.DeleteAsync(Subject.Id);
+                MessengerService.Send(new DeleteMessage<StudentDetailedModel>());
+                navigationService.SendBackButtonPressed();
+            }
+            catch (InvalidOperationException)
+            {
+                await alertService.DisplayAsync("Error", "Subject cannot be deleted.");
+            }
+        }
+    }
+
+    [RelayCommand]
+    private async Task GoToEditAsync()
+    {
+        await navigationService.GoToAsync("/edit",
+            new Dictionary<string, object?> { [nameof(SubjectEditViewModel.Subject)] = Subject });
+    }
+
+    public async void Receive(EditMessage message)
+    {
+        if (message.Id == Subject?.Id)
+        {
+            await LoadDataAsync();
+        }
+    }
+}
---
+++ SchoolSystem/SchoolSystem.App/ViewModels/Subjects/SubjectEditViewModel.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using SchoolSystem.App.Messages;
+using SchoolSystem.App.Services.Interfaces;
+using SchoolSystem.BL.Facades.Interfaces;
+using SchoolSystem.BL.Models;
+
+namespace SchoolSystem.App.ViewModels.Subjects;
+
+[QueryProperty(nameof(Subject), nameof(Subject))]
+public partial class SubjectEditViewModel(
+    ISubjectFacade subjectFacade,
+    INavigationService navigationService,
+    IMessengerService messengerService)
+    : ViewModelBase(messengerService)
+{
+    public SubjectDetailedModel Subject { get; init; } = SubjectDetailedModel.Empty;
+
+    [RelayCommand]
+    private async Task SaveAsync()
+    {
+        await subjectFacade.SaveAsync(Subject);
+
+        MessengerService.Send(new EditMessage { Id = Subject.Id });
+
+        navigationService.SendBackButtonPressed();
+    }
+}
