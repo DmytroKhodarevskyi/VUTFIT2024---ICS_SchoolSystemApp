fc848541b0cb55e41ebd80c6efcba857dda154f4
DmytroFlakac <117589874+DmytroFlakac@users.noreply.github.com>
2024-04-27 11:33:09 +0200

    BL little change

--- SchoolSystem/SchoolSystem.BL/BuissnesLogic.cs
+++ SchoolSystem/SchoolSystem.BL/BuissnesLogic.cs
@@ -1,6 +1,6 @@
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("CookBook.BL.Tests")]
+[assembly: InternalsVisibleTo("SchoolSystem.BL.Tests")]
 
 namespace SchoolSystem.BL;
 
