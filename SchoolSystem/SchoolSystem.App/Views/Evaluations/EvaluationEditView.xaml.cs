53f56a726b4d95eddb8fa7574c9fdd84968527a2
DmytroFlakac <117589874+DmytroFlakac@users.noreply.github.com>
2024-04-27 16:49:49 +0200

    EvaluationView

---
+++ SchoolSystem/SchoolSystem.App/Views/Evaluations/EvaluationEditView.xaml.cs
@@ -0,0 +1,12 @@
+using SchoolSystem.App.ViewModels.Evaluations;
+
+namespace SchoolSystem.App.Views.Evaluations;
+
+public partial class EvaluationEditView
+{
+    public EvaluationEditView(EvaluationEditViewModel viewModel)
+        : base(viewModel)
+    {
+        InitializeComponent();
+    }
+}
---
+++ SchoolSystem/SchoolSystem.App/Views/Evaluations/EvaluationListView.xaml.cs
@@ -0,0 +1,14 @@
+using SchoolSystem.App.ViewModels.Evaluations;
+
+namespace SchoolSystem.App.Views.Evaluations
+{
+    public partial class EvaluationListView
+    {
+        public EvaluationListView(EvaluationListViewModel viewModel)
+            : base(viewModel)
+        {
+            InitializeComponent();
+        }
+    }
+}
+
