c65592ba9627b973c3496bdbc5f5b651fcc371df
DenyysChernenko <xchern08@stud.fit.vutbr.cz>
2024-05-01 21:11:18 +0200

    fix(ActivityEditView) : Fixed bug with Actiivty Edit Room

--- SchoolSystem/SchoolSystem.App/ViewModels/Activity/ActivityEditViewModel.cs
+++ SchoolSystem/SchoolSystem.App/ViewModels/Activity/ActivityEditViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.Input;
+using DAL.Enums;
 using SchoolSystem.App.Messages;
 using SchoolSystem.App.Services.Interfaces;
 using SchoolSystem.BL.Facades.Interfaces;
@@ -14,6 +15,9 @@ public partial class ActivityEditViewModel(
     : ViewModelBase(messengerService)
 {
     public ActivityDetailModel Activity{ get; init; } = ActivityDetailModel.Empty;
+
+    public List<Room> Rooms => Enum.GetValues(typeof(Room)).Cast<Room>().ToList();
+
 
     [RelayCommand]
     private async Task SaveAsync()
