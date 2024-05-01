b3c0a939b7ac68fc0c995476c25ea4d970662223
Denys Chernenko <xchern08@vutbr.cz>
2024-05-01 22:33:41 +0000

    Updated ModelMapperBase.cs

--- SchoolSystem/SchoolSystem.BL/Mappers/ModelMapperBase.cs
+++ SchoolSystem/SchoolSystem.BL/Mappers/ModelMapperBase.cs
@@ -1,7 +1,6 @@
 ﻿namespace SchoolSystem.BL.Mappers;
 
-// Based on ICS Cookbook ( GPL-3.0 license ): 
-// https://github.com/nesfit/ICS/tree/master?tab=GPL-3.0-1-ov-file#readme
+
 
 public abstract class
     ModelMapperBase<TEntity, TListModel, TDetailModel> : IModelMapper<TEntity, TListModel, TDetailModel>
