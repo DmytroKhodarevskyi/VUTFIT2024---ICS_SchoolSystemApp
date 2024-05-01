91844e20687f42b68a8ea93791dea7eef9fe3b05
Dmytro <xkhoda01@vutbr.cz>
2024-05-01 16:12:44 +0200

    Activity edit update, room fix

---
+++ SchoolSystem/SchoolSystem.App/Converters/EnumToListConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Maui.Controls;
+
+namespace SchoolSystem.App.Converters;
+
+public class EnumToListConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is Type enumType && enumType.IsEnum)
+        {
+            return Enum.GetNames(enumType).ToList();
+        }
+        return null;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
+
