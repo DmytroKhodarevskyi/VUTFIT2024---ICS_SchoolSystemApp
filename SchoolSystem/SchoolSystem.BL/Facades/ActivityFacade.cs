010758adfb2723263255a7fc43d6af0ebacd4593
Dmytro <xkhoda01@vutbr.cz>
2024-05-29 21:53:05 +0200

    activity search by subject fix

--- SchoolSystem/SchoolSystem.BL/Facades/ActivityFacade.cs
+++ SchoolSystem/SchoolSystem.BL/Facades/ActivityFacade.cs
@@ -52,8 +52,14 @@ public class ActivityFacade : CrudFacade<ActivityEntity, ActivityListModel, Acti
 
             if (!string.IsNullOrEmpty(Subject))
             {
-           
-                query = query.Where(e => e.Subject != null && e.Subject.Name == Subject);
+                var repository = uow.GetRepository<SubjectEntity, SubjectEntityMapper>().Get();
+
+                var subjectId = repository
+                    .Where(s => s.Abbreviation == Subject)
+                    .Select(s => s.Id)
+                    .FirstOrDefault();
+
+                query = query.Where(e => e.Subject != null && e.SubjectId == subjectId);
            
             }
         }
